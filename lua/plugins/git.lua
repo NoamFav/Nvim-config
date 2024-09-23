3cff35f3dc2453c5cae14c9811ab47d7231ae39c
noamfav <noam.favier@icloud.com>
2024-09-23 12:05:37 +0200

    added lazy git support

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,15 @@
 -- Git configuration file
 return {
+    {
+  'kdheepak/lazygit.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-lua/popup.nvim'
+  },
+  config = function()
+    vim.api.nvim_set_keymap('n', '<leader>lg', ':LazyGit<CR>', { noremap = true, silent = true })
+  end
+},
   -- Git integration via Fugitive
   {
     'tpope/vim-fugitive',
