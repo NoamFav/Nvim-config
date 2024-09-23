c99a6fa8ec2b7514f8ec19dbf9eb399255ae310f
Noam Favier <95364866+NoamFav@users.noreply.github.com>
2024-09-23 13:08:50 +0200

    added foldable classes

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,5 +3,12 @@ return { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
 ensure_installed = "all",
 highlight = {
 	enable = true,
-}
+},
+fold = {
+    enable = true,
+    config = function()
+        vim.cmd('set foldmethod=expr')
+        vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
+        vim.o.foldlevel = 99 -- Automatically unfold everything on startup
+    end
 }
