4b6990b3e4d2faf5e1274d8406d508766e648b30
Noam Favier <95364866+NoamFav@users.noreply.github.com>
2024-09-25 11:17:14 +0200

    modified telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,16 @@
 return { 'nvim-telescope/telescope.nvim',
 	dependencies = {'nvim-lua/plenary.nvim'},
 	config = function()
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-o>"] = require('telescope.actions').select_default + require('telescope.actions').center,
+                ["<C-s>"] = require('telescope.actions').select_horizontal,
+                ["<Tab>"] = require('telescope.actions').toggle_selection + require('telescope.actions').move_selection_next,
+                ["<S-Tab>"] = require('telescope.actions').toggle_selection + require('telescope.actions').move_selection_previous,
+            }
+        }
+    }
 	require('telescope').setup{
   		defaults = {
     		prompt_prefix = "> ",
