49726688603b3ce222e51ed0608355b54ff84277
noamfav <noam.favier@icloud.com>
2024-10-04 23:32:50 +0200

    added rainbow bracket and var

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,59 @@
   -- Syntax Highlighting & Language Support (Treesitter)
-return { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
-ensure_installed = "all",
-highlight = {
-	enable = true,
-},
-fold = {
-    enable = true,
+return {
+  -- Treesitter with rainbow coloring
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    ensure_installed = "all",
     config = function()
-        vim.cmd('set foldmethod=expr')
-        vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
-        vim.o.foldlevel = 99 -- Automatically unfold everything on startup
+      require('nvim-treesitter.configs').setup {
+        highlight = {
+          enable = true,
+          custom_captures = {
+            -- Capture variables for custom highlights
+            ["variable"] = "TSRainbow1", -- Change this to TSRainbow1, TSRainbow2, etc.
+         },
+          additional_vim_regex_highlighting = false,
+        },
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Highlight more than just brackets
+          max_file_lines = nil, -- No file line limit
+        },
+        fold = {
+          enable = true,
+          config = function()
+            vim.cmd('set foldmethod=expr')
+            vim.cmd('set foldexpr=nvim_treesitter#foldexpr()')
+            vim.o.foldlevel = 99 -- Automatically unfold everything on startup
+          end,
+        },
+      }
+    end,
+  },
+
+  -- Rainbow plugin for variable highlighting
+  {
+    'p00f/nvim-ts-rainbow',
+    requires = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Also highlight variables
+        },
+      }
     end
+  },
+
+  -- Optional: Add additional treesitter-based functionality
+  {
+    'romgrk/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup {
+        enable = true, -- Enable this plugin (Can be manually disabled later)
+      }
+    end,
+  },
 }
-}
+
