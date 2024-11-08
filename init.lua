a8be04c3f3904a121360a229c3bb7cc6471464b1
noamfav <noam.favier@icloud.com>
2024-11-08 11:44:49 +0100

    added harpoon config and formatter for tsx and jsx

--- init.lua
+++ init.lua
@@ -24,3 +24,49 @@ vim.diagnostic.config({
 	update_in_insert = true,
 	severity_sort = true,
 })
+
+local harpoon = require("harpoon")
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+-- Add a file to Harpoon's quick access list
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>a",
+	"<cmd>lua require('harpoon.mark').add_file()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Open Harpoon's quick access menu
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>h",
+	"<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
+	{ noremap = true, silent = true }
+)
+
+-- Navigate to files by Harpoon index
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>1",
+	"<cmd>lua require('harpoon.ui').nav_file(1)<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>2",
+	"<cmd>lua require('harpoon.ui').nav_file(2)<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>3",
+	"<cmd>lua require('harpoon.ui').nav_file(3)<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>4",
+	"<cmd>lua require('harpoon.ui').nav_file(4)<CR>",
+	{ noremap = true, silent = true }
+)
