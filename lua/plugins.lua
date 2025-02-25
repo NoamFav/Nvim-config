434d2ddf6c3301e1f425eca2e3aa79062eb77ff8
noamfav <95364866+NoamFav@users.noreply.github.com>
2025-02-25 18:55:50 +0100

    minor updates

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,38 +1,4 @@
 return {
-	-- Color scheme
-	{
-		"navarasu/onedark.nvim",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
-	{
-		"akai54/2077.nvim",
-	},
-	{
-		"sainnhe/sonokai",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
 
 	{ "maxmellon/vim-jsx-pretty" },
 
