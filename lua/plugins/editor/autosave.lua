9cf8ae9422d755ba4707ba6f144e3574cb4cce43
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-10-28 22:14:47 +0100

    feat(main): Update autosave configuration for auto-save.nvim plugin

--- lua/plugins/editor/autosave.lua
+++ lua/plugins/editor/autosave.lua
@@ -1,23 +1,34 @@
 return {
-	"Pocco81/auto-save.nvim",
-	event = { "InsertLeave", "TextChanged" },
-	opts = {
-		enabled = true,
-		execution_message = {
-			message = function()
-				return "  AutoSave: saved at fucking " .. vim.fn.strftime("%H:%M:%S")
-			end,
-			dim = 0.18,
-			cleaning_interval = 250,
-		},
-		trigger_events = { "InsertLeave" },
-		conditions = {
-			exists = true,
-			filename_is_not = {},
-			filetype_is_not = {},
-			modifiable = true,
-		},
-		write_all_buffers = false,
-		debounce_delay = 135,
+	{
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				enabled = true,
+				execution_message = {
+					message = function()
+						return "AutoSave: saved at fucking " .. vim.fn.strftime("%H:%M:%S")
+					end,
+
+					dim = 0.18,
+					cleaning_interval = 250,
+				},
+				events = { "BufLeave" },
+				conditions = {
+					exists = true,
+					filename_is_not = {},
+					filetype_is_not = {},
+					modifiable = true,
+				},
+				write_all_buffers = false,
+				debounce_delay = 135,
+				callbacks = {
+					enabling = nil,
+					disabling = nil,
+					before_asserting_save = nil,
+					before_saving = nil,
+					after_saving = nil,
+				},
+			})
+		end,
 	},
 }
