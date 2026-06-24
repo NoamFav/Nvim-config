bf4e54ba198cf7fa3c3c0dd92f8aaf906b767b66
NoamFav <95364866+NoamFav@users.noreply.github.com>
2026-06-24 02:16:45 +0200

    feat(dependencies): update package dependencies for 2077.nvim, 42-header.nvim, LuaSnip, Zarya.nvi...

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -33,3 +33,12 @@ autocmd("BufWritePre", {
 	pattern = "*",
 	command = [[%s/\s\+$//e]],
 })
+
+autocmd("FileType", {
+	pattern = { "c", "h" },
+	callback = function()
+		vim.opt_local.expandtab = false
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
